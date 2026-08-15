aa66c63fb703c7f9d7aea6604ee7c0b921d9cb8c
AdityaSaaspect <aditya.pundarika@gmail.com>
2026-08-15 15:36:39 +0530

    postgresql shell script

--- postgresql.sh
+++ postgresql.sh
@@ -11,5 +11,5 @@ cp pg_hba.conf /var/lib/pgsql/16/data/pg_hba.conf
 
 systemctl restart postgresql-16
 
-postgres /usr/pgsql-16/bin/psql -f schema.sql
+sudo -u postgres /usr/pgsql-16/bin/psql -f schema.sql
 
